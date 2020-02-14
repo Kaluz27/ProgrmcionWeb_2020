dbb4e73f7de82264497a1ea700d9f71d0fe67ecb
Kaluz27 <eluna3@ucol.mx>
2020-02-13 19:20:47 -0600

    Practica

--- Miercoles_12_02_20/Funciones/app.js
+++ Miercoles_12_02_20/Funciones/app.js
@@ -1,15 +1,80 @@
 //Funtion statement
-let greet = () => {
+/*let greet = () => {
     console.log('hi');
 }
 greet();
 
 //funtion are firt-class
-let longGreeting = (fn) =>{
+/*let longGreeting = (fn) =>{
 fn()
 }
 longGreeting();
 
 //Funtion expression on the fly
 
-longGreeting=(() => {console.log('Hi')} );
+let longGreeting=(() => {console.log('Hi')} );
+longGreeting()
+*/
+// ----------------------------------------------------------------------------------------------------------------------------
+// Decalaracion de funciones
+
+// function greet(){
+//     console.log('hi');
+// }
+// greet();
+
+
+// Declaracion de funcion (arrow -> fuction)
+
+// let greet = () => {
+//     console.log('Hi');
+// }
+// greet();
+
+
+// Declaracion de funcion 
+
+// logGreeting(greet);
+
+// function logGreeting(fn){
+//     fn();
+// }
+
+// function greet(){
+//     console.log('hi');
+// }
+
+// string literals
+
+// let hola = (() => {
+//     console.log("hello")
+// });
+// console.log(`${hola} corazon como estas?`)
+
+
+// Declaracion de funciones nivel 4 
+
+// function greet(){
+//     console.log('hi');
+// }
+
+// function logGreeting(fn){
+//     fn();
+// }
+
+// let logGreeting = (fn) => {
+//     fn();
+// }
+
+// let logGreeting = fn => fn();
+
+// logGreeting(greet);
+
+
+// functions expressions on the fly
+
+let logGreeting = (() => {
+    console.log('Hi');
+});
+
+logGreeting();
