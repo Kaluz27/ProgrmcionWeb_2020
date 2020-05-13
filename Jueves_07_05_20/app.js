6d489e89bcb2dbd265d83f98089ed7a7a2b273b8
Kaluz27 <eluna3@ucol.mx>
2020-05-12 22:22:02 -0500

    Actividad de hoy

--- Jueves_07_05_20/app.js
+++ Jueves_07_05_20/app.js
@@ -1,22 +1,39 @@
 const express = require('express');
 
 const app = express();
-
+const bodyParser = require('body-parser');
 const port = 3000;
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.post('/person', urlencodedParser, (req, res) => {
+    res.send('Thanks');
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);
+});
+
+var jsonParser = bodyParser.json();
+app.post('/personjson', jsonParser, (req, res) => {
+    res.send('Thanks from jsonParser');
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);
+
+});
 
 //Evitar revelar el contenido de los directorios a nuestro servidor
 app.use(('/assets'), express.static(__dirname + '/public'));
 app.set('view engine','ejs');
 
-//route handler para la raiz
-/*app.get('/', (req,res)=>{
-    res.render('index');
-    //send('<html><head> <link href=/assets/style.css type=text/css rel=stylesheet> </head><title>Page title</title><body><h1>My first Heading</h1><p>My first paragraph</p></body></html>');
-})*/
+app.get('/person/:id', (req, res) => {
+    res.render('person', {
+    ID: req.params.id,
+    Qstr: req.query.Qstr
+    });});
 
-app.get('/person/:id', (req,res)=>{
-    res.render('index', { ID: req.params.id})    
-});
+
+    app.get('/', (req, res) => {
+        res.render('personData');
+    });
+    
 
 app.listen(port, ()=>{
     console.log("Escuchando en el puerto: "+port);
